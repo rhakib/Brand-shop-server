2a5581af7043a2459d2c43f481fc4e17860f1cbc
Rakibul Hasan <87831331+rhakib@users.noreply.github.com>
2023-10-19 22:57:00 +0600

    delete function done for carts products

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -81,6 +81,13 @@ async function run() {
       const cursor = cartDatabase.find()
       const result = await cursor.toArray()
       res.send(result)
+  })
+
+  app.delete('/cart/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const result = await cartDatabase.deleteOne(query)
+    res.send(result)
   })
 
     // Send a ping to confirm a successful connection
